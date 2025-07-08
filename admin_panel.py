13f3d97a087d64b0d03edfa21bec2c603f4a7b7f
zakco <Zakcoyote@gmail.com>
2025-07-08 15:53:01 +0300

    Добавлены две кнопки Скачать CSV и Excel

--- admin_panel.py
+++ admin_panel.py
@@ -1,8 +1,96 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from database import session, Message
 from translations import translations
+from flask import send_file
+import pandas as pd
+from io import BytesIO, StringIO
+
 
 app = Flask(__name__)
+
+@app.route('/export/csv')
+def export_csv():
+    selected_group = request.args.get('group')
+    query = session.query(Message)
+    if selected_group:
+        query = query.filter(Message.group_name == selected_group)
+    messages = query.all()
+
+    data = [{
+        'group': m.group_name,
+        'date': m.date,
+        'sender': m.sender_id,
+        'text': m.text,
+        'media': m.media_path
+    } for m in messages]
+
+    df = pd.DataFrame(data)
+
+    # Сохраняем в байтовый буфер с правильной кодировкой utf-8-sig
+    buffer = BytesIO()
+    buffer.write(df.to_csv(index=False, encoding='utf-8-sig').encode('utf-8-sig'))
+    buffer.seek(0)
+
+    return send_file(
+        buffer,
+        mimetype='text/csv',
+        as_attachment=True,
+        download_name='messages.csv'
+    )
+
+
+@app.route('/export/excel')
+def export_excel():
+    selected_group = request.args.get('group')
+    query = session.query(Message)
+    if selected_group:
+        query = query.filter(Message.group_name == selected_group)
+    messages = query.all()
+
+    data = [{
+        'group': m.group_name,
+        'date': m.date,
+        'sender': m.sender_id,
+        'text': m.text,
+        'media': m.media_path
+    } for m in messages]
+
+    df = pd.DataFrame(data)
+
+    buffer = BytesIO()
+    with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Messages')
+        worksheet = writer.sheets['Messages']
+
+        media_col = 4
+
+        for row_num, media_paths in enumerate(df['media'], start=1):
+            if media_paths:
+                paths = media_paths.split(',')
+                first_path = paths[0].strip().replace('\\', '/')
+
+                # Создаем веб-ссылку на файл
+                # 'static/media/filename.jpg' -> 'media/filename.jpg' (если нужно)
+                # Обычно в url_for указывается путь после 'static/', поэтому:
+                static_path = first_path
+                if static_path.startswith('static/'):
+                    static_path = static_path[len('static/'):]  # отрезаем 'static/'
+
+                url = request.host_url.rstrip('/') + url_for('static', filename=static_path)
+
+                worksheet.write_url(row_num, media_col, url, string='PNG')
+            else:
+                worksheet.write(row_num, media_col, '-')
+
+    buffer.seek(0)
+
+    return send_file(
+        buffer,
+        mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        as_attachment=True,
+        download_name='messages.xlsx'
+    )
+
 
 @app.route('/')
 def home():
---
+++ config.py
@@ -0,0 +1,3 @@
+api_id = 20045675
+api_hash = "3e16868e53ce20f332dd9c87f6887d6d"
+group_usernames = ["@FlutterDartVader"]
